f4b9d0f65920fc6a0eae5c53c8a4c47314ce99c3
David Weber <dsw7@sfu.ca>
2023-11-01 23:25:32 -0700

    Add a basic help message

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <string>
 
+void help(const std::string &exec)
+{
+    std::cerr << "--- GPTifier ---\n";
+    std::cerr << "Usage: " + exec + " '<prompt>'" << std::endl;
+}
+
 int main(int argc, char **argv)
 {
+    std::string exec = std::string(argv[0]);
+
     if (argc < 2)
     {
-        std::cerr << "Usage: " + std::string(argv[0]) + " '<prompt>'" << std::endl;
+        ::help(exec);
         return EXIT_FAILURE;
     }
 
-    return 0;
+    std::string prompt = std::string(argv[1]);
+
+    if (prompt.compare("-h") == 0)
+    {
+        ::help(exec);
+        return EXIT_SUCCESS;
+    }
+
+    if (prompt.compare("--help") == 0)
+    {
+        ::help(exec);
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
 }
