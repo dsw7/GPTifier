84988c20eeeb9e7842237c58c9778d2359c84a4c
David Weber <dsw7@sfu.ca>
2023-11-02 00:07:27 -0700

    Load API key

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
+#include <cstdlib>
+#include <curl/curl.h>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 void help(const std::string &exec)
 {
     std::cerr << "--- GPTifier ---\n";
     std::cerr << "Usage: " + exec + " '<prompt>'" << std::endl;
+}
+
+void load_api_key(std::string &api_key)
+{
+    const char* home_dir = std::getenv("HOME");
+
+    if (not home_dir)
+    {
+        throw std::runtime_error("Could not locate home directory!");
+    }
+
+    std::string path_key = std::string(home_dir) + "/.openai";
+
+    if (not std::filesystem::exists(path_key))
+    {
+        throw std::runtime_error("Could not locate .openai key file in home directory!");
+    }
+
+    std::ifstream s_key(path_key);
+
+    while (std::getline(s_key, api_key))
+    {
+        std::cout << api_key;
+    }
 }
 
 int main(int argc, char **argv)
@@ -29,6 +58,18 @@ int main(int argc, char **argv)
     {
         ::help(exec);
         return EXIT_SUCCESS;
+    }
+
+    std::string api_key;
+
+    try
+    {
+        ::load_api_key(api_key);
+    }
+    catch (std::runtime_error& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
