c211483eb52ac22cacbfc932a2f2eb9a84efdd05
David Weber <dsw7@sfu.ca>
2023-11-05 02:23:14 -0800

    First compilable iteration

--- src/configs.cpp
+++ src/configs.cpp
@@ -1,9 +1,26 @@
 #include "configs.h"
 
 #include <toml++/toml.hpp>
+#include <cstdlib> 
+#include <filesystem>
+#include <stdexcept>
 
 void read_configs(Configs &configs)
 {
+    const char* home_dir = std::getenv("HOME");
+
+    if (not home_dir)
+    {
+        throw std::runtime_error("Could not locate home directory!");
+    }
+
+    std::string path_key = std::string(home_dir) + "/.gptifier";
+
+    if (not std::filesystem::exists(path_key))
+    {
+        throw std::runtime_error("Could not locate .gptifier TOML file in home directory!");
+    }
+
     configs.api_key = "foo";
     configs.model = "foo";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include "query.h"
+#include "configs.h"
 #include "presentation.h"
+#include "query.h"
 
-#include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <stdexcept>
@@ -13,28 +12,6 @@ void read_prompt(std::string &prompt)
     presentation::print_separator();
     std::cout << "\033[1mInput:\033[0m ";
     std::getline(std::cin, prompt);
-}
-
-void load_api_key(std::string &api_key)
-{
-    const char* home_dir = std::getenv("HOME");
-
-    if (not home_dir)
-    {
-        throw std::runtime_error("Could not locate home directory!");
-    }
-
-    std::string path_key = std::string(home_dir) + "/.openai";
-
-    if (not std::filesystem::exists(path_key))
-    {
-        throw std::runtime_error("Could not locate .openai key file in home directory!");
-    }
-
-    std::ifstream s_key(path_key);
-    std::getline(s_key, api_key);
-
-    s_key.close();
 }
 
 void build_payload(const std::string &prompt, std::string &payload)
@@ -53,24 +30,24 @@ void build_payload(const std::string &prompt, std::string &payload)
 
 int main()
 {
+    Configs configs;
+
+    try
+    {
+        ::read_configs(configs);
+    }
+    catch (std::runtime_error& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
     std::string prompt;
     ::read_prompt(prompt);
 
     if (prompt.empty())
     {
         std::cerr << "Prompt cannot be empty" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::string api_key;
-
-    try
-    {
-        ::load_api_key(api_key);
-    }
-    catch (std::runtime_error& e)
-    {
-        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -82,7 +59,7 @@ int main()
 
     try
     {
-        QueryHandler q(api_key);
+        QueryHandler q(configs.api_key);
         q.run_query(payload, reply);
     }
     catch (std::runtime_error& e)
