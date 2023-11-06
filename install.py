fca1c4046351142c38e2e41c5ba40b5dfe53f340
David Weber <dsw7@sfu.ca>
2023-11-05 19:58:28 -0800

    Temp dir + downloader

--- install.py
+++ install.py
@@ -1,5 +1,17 @@
+import sys
 from argparse import ArgumentParser
 from pathlib import Path
+from tempfile import gettempdir, TemporaryDirectory
+from requests import get
+
+
+def download_file(url: str, file_path: Path) -> None:
+    response = get(url, stream=True)
+
+    with open(file_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
 
 
 def main() -> None:
@@ -8,7 +20,12 @@ def main() -> None:
 
     args = parser.parse_args()
     include_path = Path(args.include_path)
-    print(include_path)
+
+    if not include_path.exists():
+        sys.exit(f"Path {include_path} does not exist")
+
+    with TemporaryDirectory() as td:
+        print(td)
 
 
 if __name__ == "__main__":
