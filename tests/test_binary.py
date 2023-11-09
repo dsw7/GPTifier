df0baeed0923b492a60cc80de7adf0601c232c5c
David Weber <dsw7@sfu.ca>
2023-11-09 04:47:48 -0800

    Write to tempdir

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -1,12 +1,20 @@
 from json import loads
 from pathlib import Path
 from subprocess import run, CalledProcessError, PIPE, DEVNULL
-from tempfile import gettempdir
-from pytest import fail
+from tempfile import TemporaryDirectory
+from typing import Generator
+from pytest import fail, fixture
 
 
-def test_binary() -> None:
-    json_file = Path(gettempdir()) / "gptifier_test.json"
+@fixture
+def tempdir() -> Generator[Path, None, None]:
+    t_dir = TemporaryDirectory()
+    yield Path(t_dir.name)
+    t_dir.cleanup()
+
+
+def test_binary(tempdir: Path) -> None:
+    json_file = tempdir / "test.json"
 
     command = [
         "build/gpt",
