e1bfb1bbb6a99c3445de125189b01996ec71c406
David Weber <dsw7@sfu.ca>
2023-11-23 23:08:29 -0800

    Add unit tests

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from subprocess import run, CalledProcessError, PIPE, DEVNULL
 from tempfile import TemporaryDirectory
 from typing import Generator
-from pytest import fail, fixture
+from pytest import fail, fixture, mark
 
 
 @fixture
@@ -13,12 +13,13 @@ def tempdir() -> Generator[Path, None, None]:
     t_dir.cleanup()
 
 
-def test_binary(tempdir: Path) -> None:
-    json_file = tempdir / "test.json"
+def test_basic(tempdir: Path) -> None:
+    json_file = tempdir / "test_basic.json"
 
     command = [
         "build/gpt",
         "--prompt='What is 3 + 5? Format the result as follows: >>>{result}<<<'",
+        "--temperature=0",
         f"--dump={json_file}",
     ]
 
@@ -30,3 +31,30 @@ def test_binary(tempdir: Path) -> None:
     with json_file.open() as f:
         data = loads(f.read())
         assert data["choices"][0]["message"]["content"] == ">>>8<<<"
+
+
+@mark.parametrize(
+    "temp, result",
+    [
+        ("-2.5", "-2.5 is less than the minimum of 0 - 'temperature'"),
+        ("2.5", "2.5 is greater than the maximum of 2 - 'temperature'"),
+    ],
+)
+def test_invalid_temp(temp: str, result: str, tempdir: Path) -> None:
+    json_file = tempdir / "test_invalid_temp.json"
+
+    command = [
+        "build/gpt",
+        "--prompt='Running a test!'",
+        f"--dump={json_file}",
+        f"--temperature={temp}",
+    ]
+
+    try:
+        run(command, stdout=DEVNULL, stderr=PIPE, check=True)
+    except CalledProcessError as exc:
+        fail(exc.stderr.decode())
+
+    with json_file.open() as f:
+        data = loads(f.read())
+        assert data["error"]["message"] == result
