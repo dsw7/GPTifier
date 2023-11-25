5a658f1763eb5b5e1375933e54cb6ba755d110ec
David Weber <dsw7@sfu.ca>
2023-11-24 16:27:54 -0800

    Add another test

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from subprocess import run, CalledProcessError, PIPE, DEVNULL
 from tempfile import TemporaryDirectory
 from typing import Generator
-from pytest import fail, fixture, mark
+import pytest
 
 
-@fixture
+@pytest.fixture
 def tempdir() -> Generator[Path, None, None]:
     t_dir = TemporaryDirectory()
     yield Path(t_dir.name)
@@ -26,14 +26,14 @@ def test_basic(tempdir: Path) -> None:
     try:
         run(command, stdout=DEVNULL, stderr=PIPE, check=True)
     except CalledProcessError as exc:
-        fail(exc.stderr.decode())
+        pytest.fail(exc.stderr.decode())
 
     with json_file.open() as f:
         data = loads(f.read())
         assert data["choices"][0]["message"]["content"] == ">>>8<<<"
 
 
-@mark.parametrize(
+@pytest.mark.parametrize(
     "temp, result",
     [
         ("-2.5", "-2.5 is less than the minimum of 0 - 'temperature'"),
@@ -53,7 +53,7 @@ def test_invalid_temp(temp: str, result: str, tempdir: Path) -> None:
     try:
         run(command, stdout=DEVNULL, stderr=PIPE, check=True)
     except CalledProcessError as exc:
-        fail(exc.stderr.decode())
+        pytest.fail(exc.stderr.decode())
 
     with json_file.open() as f:
         data = loads(f.read())
@@ -76,8 +76,15 @@ def test_read_from_file(tempdir: Path) -> None:
     try:
         run(command, stdout=DEVNULL, stderr=PIPE, check=True)
     except CalledProcessError as exc:
-        fail(exc.stderr.decode())
+        pytest.fail(exc.stderr.decode())
 
     with json_file.open() as f:
         data = loads(f.read())
         assert data["choices"][0]["message"]["content"] == ">>>8<<<"
+
+
+def test_missing_file() -> None:
+    command = ["build/gpt", "--read-from-file=/tmp/yU8nnkRs.txt"]
+
+    with pytest.raises(CalledProcessError):
+        run(command, stdout=DEVNULL, stderr=DEVNULL, check=True)
