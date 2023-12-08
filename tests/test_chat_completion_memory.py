e32b47e298565a911ea27ef4b5f717ee33c42ca9
David Weber <dsw7@sfu.ca>
2023-12-08 03:38:50 -0800

    Skip Valgrind tests on MacOS

--- tests/test_chat_completion_memory.py
+++ tests/test_chat_completion_memory.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+from platform import system
 from subprocess import run, DEVNULL, PIPE, CalledProcessError
-from pytest import mark, fail
+from pytest import mark, fail, skip
+
+if system() == "Darwin":
+    skip("Valgrind not supported on MacOS", allow_module_level=True)
 
 VALGRIND_ROOT = ["valgrind", "--error-exitcode=1", "--leak-check=full", "build/gpt"]
 
