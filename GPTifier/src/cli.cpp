fb41f1d0810b95d7d0ea167ce194608701576dad
David Weber <dsw7@sfu.ca>
2023-12-10 04:09:29 -0800

    Formatting + examples section

--- GPTifier/src/cli.cpp
+++ GPTifier/src/cli.cpp
@@ -14,14 +14,14 @@ void print_help_messages()
 
     std::string doc =
         "\033[1mNAME:\033[0m\n"
-        "     \033[4m" +
+        "   \033[4m" +
         name + " v" + version +
         "\033[0m\n\n"
         "\033[1mDESCRIPTION:\033[0m\n"
-        "     A command line program for interactively querying OpenAI via the OpenAI API. See\n"
-        "     \033[4mhttps://github.com/dsw7/GPTifier\033[0m for more information.\n\n"
+        "   A command line program for interactively querying OpenAI via the OpenAI API. See\n"
+        "   \033[4mhttps://github.com/dsw7/GPTifier\033[0m for more information.\n\n"
         "\033[1mSYNOPSIS:\033[0m\n"
-        "     \033[4mgpt\033[0m [<options>]\n\n"
+        "   \033[4mgpt\033[0m [<options>]\n\n"
         "\033[1mOPTIONS:\033[0m\n"
         "  \033[2m-h, --help\033[0m\n"
         "     Print help information and exit.\n\n"
@@ -37,7 +37,8 @@ void print_help_messages()
         "     in certain cases, such as when running automated tests. This flag disables this behavior.\n\n"
         "  \033[2m-d <json-file>, --dump=<json-file>\033[0m\n"
         "     Export results to a JSON file. This feature may be useful if performing automated\n"
-        "     unit tests.\n\n"
+        "     unit tests. This flag will implicitly disable interactive export, in a fashion\n"
+        "     similar to the \033[2m--no-interactive-export\033[0m flag.\n\n"
         "  \033[2m-p <prompt>, --prompt=<prompt>\033[0m\n"
         "     Provide prompt via command line as opposed to interactively. This feature may be\n"
         "     useful if performating automated unit tests. Note that a prompt provided via command line\n"
@@ -47,7 +48,12 @@ void print_help_messages()
         "     with complex, multiline prompts.\n\n"
         "  \033[2m-t <temp>, --temperature=<temp>\033[0m\n"
         "     Provide a sampling temperature between 0 and 2. Values approaching 0 provide more\n"
-        "     coherent completions, whereas values approaching 2 provide more creative completions.\n";
+        "     coherent completions, whereas values approaching 2 provide more creative completions.\n\n"
+        "\033[1mEXAMPLES:\033[0m\n"
+        "   1. Run an interactive session:\n"
+        "     $ gpt\n\n"
+        "   2. Run a query non-interactively and export results:\n"
+        "     $ gpt --prompt=\"What is 3 + 5\" --dump=\"/tmp/results.json\"\n";
     std::cout << doc << std::endl;
 }
 
