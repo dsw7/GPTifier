62344992ee9be0ed2db8540434d85bc3349f3ced
David Weber <dsw7@sfu.ca>
2023-12-31 21:39:53 -0800

    Rename data.hpp -> params.hpp

---
+++ GPTifier/include/params.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+struct Params
+{
+    // Parameters strictly from configuration file
+    std::string api_key;
+
+    // Parameters from command line interface
+    bool enable_export = true;
+    std::string dump;
+    std::string model = "gpt-3.5-turbo";
+    std::string prompt;
+    std::string prompt_file;
+    std::string temperature = "1";
+};
+
+extern Params params;
