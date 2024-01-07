9ccc3daf4dc9ba1b04ab90c3c9e01c4702d152e8
David Weber <dsw7@sfu.ca>
2024-01-06 20:03:33 -0800

    Add cli switch for enabling memory tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,26 @@ from typing import Generator
 from pytest import fixture
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--memory", action="store_true", default=False, help="Run Valgrind tests"
+    )
+
+
+@fixture(scope="session")
+def command(pytestconfig) -> list[str]:
+    if not pytestconfig.getoption("memory"):
+        return ["build/gpt", "--no-interactive-export"]
+
+    return [
+        "valgrind",
+        "--error-exitcode=1",
+        "--leak-check=full",
+        "build/gpt",
+        "--no-interactive-export",
+    ]
+
+
 @fixture
 def tempdir() -> Generator[Path, None, None]:
     handle_tempdir = TemporaryDirectory()
