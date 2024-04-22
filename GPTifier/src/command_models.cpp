d385a6e2bd447914b70d11b7afebe4b7340af470
David Weber <dds3443@hotmail.com>
2024-04-22 04:39:44 -0700

    Add API call

--- GPTifier/src/command_models.cpp
+++ GPTifier/src/command_models.cpp
@@ -1,5 +1,34 @@
 #include "command_models.hpp"
 
+#include "api.hpp"
+
+#include <curl/curl.h>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+void get_models(::CURL *curl, std::string &response)
+{
+    static std::string url_get_models = "https://api.openai.com/v1/models";
+    ::curl_easy_setopt(curl, ::CURLOPT_URL, url_get_models.c_str());
+
+    ::curl_easy_setopt(curl, ::CURLOPT_HTTPGET, 1L);
+    ::curl_easy_setopt(curl, ::CURLOPT_WRITEDATA, &response);
+
+    ::CURLcode rv = ::curl_easy_perform(curl);
+    if (rv != ::CURLE_OK)
+    {
+        std::string errmsg = "Failed to run query. " + std::string(::curl_easy_strerror(rv));
+        throw std::runtime_error(errmsg);
+    }
+}
+
 void command_models()
 {
+    Curl curl;
+    std::string response;
+
+    ::get_models(curl.handle, response);
+
+    std::cout << response;
 }
