0d13ec7e40e272cc45ab362947cfa6f27833f2e0
David Weber <dds3443@hotmail.com>
2024-04-24 00:25:15 -0700

    Add help message test for models

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,16 @@ from os import EX_OK
 from subprocess import run
 
 
+def test_models_help(command: list[str], capfd) -> None:
+    command.extend(["models", "--help"])
+
+    process = run(command)
+    assert process.returncode == EX_OK
+
+    cap = capfd.readouterr()
+    assert "SYNOPSIS" in cap.out
+
+
 def test_models(command: list[str], capfd) -> None:
     command.extend(["models"])
 
