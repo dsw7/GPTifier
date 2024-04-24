dbd8562014b683a50eb75b348f2ee2760a136cb3
David Weber <dds3443@hotmail.com>
2024-04-23 23:34:57 -0700

    Remove params struct from params

--- GPTifier/include/params.hpp
+++ GPTifier/include/params.hpp
@@ -6,22 +6,7 @@ struct Configs
 {
     void load_configs_from_config_file();
 
-    // Parameters strictly from configuration file
     std::string api_key;
 };
 
-struct Params
-{
-    void load_params_from_command_line(const int argc, char **argv);
-
-    // Parameters from command line interface
-    bool enable_export = true;
-    std::string dump;
-    std::string model = "gpt-3.5-turbo";
-    std::string prompt;
-    std::string prompt_file;
-    std::string temperature = "1";
-};
-
-extern Params params;
 extern Configs configs;
