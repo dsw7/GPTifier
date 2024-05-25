f9465b20a523c8f454d2d3133a43b7562a69e7db
dsw7 <dds3443@hotmail.com>
2024-05-25 01:29:02 -0700

    Remove API key from configs struct

--- GPTifier/src/configs.cpp
+++ GPTifier/src/configs.cpp
@@ -25,7 +25,6 @@ void Configs::load_configs_from_config_file()
         throw std::runtime_error(e);
     }
 
-    this->api_key = table["authentication"]["api-key"].value_or("");
     this->model = table["profile"]["model"].value_or("");
     this->project_id = table["profile"]["project-id"].value_or("");
 }
