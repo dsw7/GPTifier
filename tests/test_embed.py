f1a4638ad3cda953871c1aeae4e4bd52b2685977
dsw7 <dds3443@hotmail.com>
2024-06-01 22:28:13 -0700

    Add help message test

---
+++ tests/test_embed.py
@@ -0,0 +1,16 @@
+from os import EX_OK
+from subprocess import run
+from utils import print_stdout, print_stderr
+
+
+def test_embed_help(command: list[str], capfd) -> None:
+    command.extend(["models", "--help"])
+    process = run(command)
+
+    capture = capfd.readouterr()
+    print()
+    print_stdout(capture.out)
+    print_stderr(capture.err)
+
+    assert process.returncode == EX_OK
+    assert "SYNOPSIS" in capture.out
