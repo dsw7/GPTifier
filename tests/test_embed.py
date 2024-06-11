99010486ba6fb6815e8d5df253ebd207fcd796b5
dsw7 <dds3443@hotmail.com>
2024-06-11 00:19:04 -0700

    Add bad model test

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from subprocess import run
 from utils import print_stdout, print_stderr, print_stdout_stderr
 
+RESULTS_JSON = Path.home() / ".gptifier" / "embeddings.gpt"
+
 
 def test_basic(command: list[str], capfd) -> None:
     model = "text-embedding-ada-002"
@@ -15,12 +17,25 @@ def test_basic(command: list[str], capfd) -> None:
     print_stdout_stderr(capfd)
     assert process.returncode == EX_OK
 
-    results_json = Path.home() / ".gptifier" / "embeddings.gpt"
-
-    with open(results_json) as f_json:
+    with open(RESULTS_JSON) as f_json:
         data = loads(f_json.read())
         assert data["model"] == model
         assert len(data["data"][0]["embedding"]) == ada_002_vector_dimension
+
+
+def test_invalid_model(command: list[str], capfd) -> None:
+    command.extend(["embed", "-i'What is 3 + 5?'", "-mfoobar"])
+    process = run(command)
+
+    print_stdout_stderr(capfd)
+    assert process.returncode == EX_OK
+
+    with open(RESULTS_JSON) as f_json:
+        data = loads(f_json.read())
+        assert (
+            data["error"]["message"]
+            == "The model `foobar` does not exist or you do not have access to it."
+        )
 
 
 def test_embed_help(command: list[str], capfd) -> None:
