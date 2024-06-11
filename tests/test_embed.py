55d6a12da939bbf4bb634c4b61275d72d4a81e36
dsw7 <dds3443@hotmail.com>
2024-06-11 00:13:41 -0700

    Ordering

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -5,19 +5,6 @@ from subprocess import run
 from utils import print_stdout, print_stderr, print_stdout_stderr
 
 
-def test_embed_help(command: list[str], capfd) -> None:
-    command.extend(["embed", "--help"])
-    process = run(command)
-
-    capture = capfd.readouterr()
-    print()
-    print_stdout(capture.out)
-    print_stderr(capture.err)
-
-    assert process.returncode == EX_OK
-    assert "SYNOPSIS" in capture.out
-
-
 def test_basic(command: list[str], capfd) -> None:
     model = "text-embedding-ada-002"
     ada_002_vector_dimension = 1536
@@ -34,3 +21,16 @@ def test_basic(command: list[str], capfd) -> None:
         data = loads(f_json.read())
         assert data["model"] == model
         assert len(data["data"][0]["embedding"]) == ada_002_vector_dimension
+
+
+def test_embed_help(command: list[str], capfd) -> None:
+    command.extend(["embed", "--help"])
+    process = run(command)
+
+    capture = capfd.readouterr()
+    print()
+    print_stdout(capture.out)
+    print_stderr(capture.err)
+
+    assert process.returncode == EX_OK
+    assert "SYNOPSIS" in capture.out
