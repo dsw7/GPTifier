3f3bafe8c9a84c54c5670a74db03db567c65ef5d
dsw7 <dds3443@hotmail.com>
2024-06-11 00:09:27 -0700

    Add basic embedding test

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,6 +1,8 @@
+from json import loads
 from os import EX_OK
+from pathlib import Path
 from subprocess import run
-from utils import print_stdout, print_stderr
+from utils import print_stdout, print_stderr, print_stdout_stderr
 
 
 def test_embed_help(command: list[str], capfd) -> None:
@@ -14,3 +16,24 @@ def test_embed_help(command: list[str], capfd) -> None:
 
     assert process.returncode == EX_OK
     assert "SYNOPSIS" in capture.out
+
+
+def test_basic(command: list[str], capfd) -> None:
+    command.extend(
+        [
+            "embed",
+            "-i'What is 3 + 5?'",
+            "-mtext-embedding-ada-002",
+        ]
+    )
+    process = run(command)
+
+    print_stdout_stderr(capfd)
+    assert process.returncode == EX_OK
+
+    results_json = Path.home() / ".gptifier" / "embeddings.gpt"
+
+    with open(results_json) as f_json:
+        data = loads(f_json.read())
+        assert data["model"] == "text-embedding-ada-002"
+        assert len(data["data"][0]["embedding"]) == 1536
