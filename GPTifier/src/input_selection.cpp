aba47b8ee554fe398b38f4bb79dfc79795a94e58
dsw7 <dds3443@hotmail.com>
2024-07-29 00:55:03 -0700

    Read from Inputfile

--- GPTifier/src/input_selection.cpp
+++ GPTifier/src/input_selection.cpp
@@ -2,23 +2,56 @@
 
 #include "utils.hpp"
 
+#include <filesystem>
 #include <iostream>
 #include <stdexcept>
 
-void load_input_text(std::string &input, const std::string &input_file)
+void get_text_from_cli_specified_file(std::string &text, const std::string &filename)
 {
-    // Input text was passed via file
-    if (not input_file.empty())
+    ::read_text_from_file(filename, text);
+}
+
+void get_text_from_inputfile(std::string &text)
+{
+    static std::filesystem::path inputfile = std::filesystem::current_path() / "Inputfile";
+
+    if (not std::filesystem::exists(inputfile))
     {
-        ::read_text_from_file(input_file, input);
         return;
     }
 
-    // Otherwise default to reading the input text from stdin
-    std::cout << "\033[1mInput:\033[0m ";
-    std::getline(std::cin, input);
+    if (std::filesystem::is_empty(inputfile))
+    {
+        return;
+    }
 
-    // If still empty then we cannot proceed
+    std::cout << "Found an Inputfile in current working directory!\n";
+    ::read_text_from_file(inputfile, text);
+}
+
+void get_text_from_stdin(std::string &text)
+{
+    std::cout << "\033[1mInput:\033[0m ";
+    std::getline(std::cin, text);
+}
+
+void load_input_text(std::string &input, const std::string &input_file)
+{
+    if (not input_file.empty())
+    {
+        ::get_text_from_cli_specified_file(input, input_file);
+        return;
+    }
+
+    ::get_text_from_inputfile(input);
+
+    if (not input.empty())
+    {
+        return;
+    }
+
+    ::get_text_from_stdin(input);
+
     if (input.empty())
     {
         throw std::runtime_error("No input text provided anywhere. Cannot proceed");
