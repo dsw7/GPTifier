f7e0a6c2db9f0deab20a32150d9d505f50d65984
dsw7 <dds3443@hotmail.com>
2024-08-01 23:35:25 -0700

    Add test logger

--- GPTifier/src/testing.cpp
+++ GPTifier/src/testing.cpp
@@ -11,7 +11,7 @@ bool is_pytest_running()
 
     if (pytest_current_test)
     {
-        std::cout << fmt::format("Detected pytest run ({})\n", pytest_current_test);
+        std::cout << fmt::format("[TEST] Detected pytest run ({})\n", pytest_current_test);
         return true;
     }
 
@@ -22,4 +22,14 @@ bool is_test_running()
 {
     static bool pytest_running = ::is_pytest_running();
     return pytest_running;
+}
+
+void log_test(const std::string &message)
+{
+    static bool pytest_running = ::is_pytest_running();
+
+    if (pytest_running)
+    {
+        std::cout << fmt::format("[TEST] {}\n", message);
+    }
 }
