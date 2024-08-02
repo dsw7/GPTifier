1d27e9e0a1503f9ba30667e38476a0195f79d3ea
dsw7 <dds3443@hotmail.com>
2024-08-01 23:56:28 -0700

    Call logs

--- GPTifier/src/input_selection.cpp
+++ GPTifier/src/input_selection.cpp
@@ -1,5 +1,6 @@
 #include "input_selection.hpp"
 
+#include "testing.hpp"
 #include "utils.hpp"
 
 #include <filesystem>
@@ -8,6 +9,7 @@
 
 void get_text_from_cli_specified_file(std::string &text, const std::string &filename)
 {
+    ::log_test("Loaded input text from custom file");
     ::read_text_from_file(filename, text);
 }
 
@@ -25,12 +27,14 @@ void get_text_from_inputfile(std::string &text)
         return;
     }
 
+    ::log_test("Loaded input text from Inputfile");
     std::cout << "Found an Inputfile in current working directory!\n";
     ::read_text_from_file(inputfile, text);
 }
 
 void get_text_from_stdin(std::string &text)
 {
+    ::log_test("Loaded input text from stdin");
     std::cout << "\033[1mInput:\033[0m ";
     std::getline(std::cin, text);
 }
