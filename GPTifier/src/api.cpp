9480372e85a61483c51c6dc5ec27365c85bf9f3e
dsw7 <dds3443@hotmail.com>
2024-09-01 02:14:14 -0700

    Move local functions to anonymous namespace for privacy:

--- GPTifier/src/api.cpp
+++ GPTifier/src/api.cpp
@@ -5,6 +5,9 @@
 #include <stdexcept>
 #include <string>
 
+namespace
+{
+
 size_t write_callback(char *ptr, size_t size, size_t nmemb, std::string *data)
 {
     data->append(ptr, size * nmemb);
@@ -23,21 +26,23 @@ const std::string get_api_key()
     return std::string();
 }
 
+} // namespace
+
 Curl::Curl()
 {
-    if (::curl_global_init(CURL_GLOBAL_DEFAULT) != 0)
+    if (curl_global_init(CURL_GLOBAL_DEFAULT) != 0)
     {
         throw std::runtime_error("Something went wrong when initializing libcurl");
     }
 
-    std::string api_key = ::get_api_key();
+    std::string api_key = get_api_key();
 
     if (api_key.empty())
     {
         throw std::runtime_error("OPENAI_API_KEY environment variable not set");
     }
 
-    this->handle = ::curl_easy_init();
+    this->handle = curl_easy_init();
 
     if (this->handle == NULL)
     {
@@ -45,27 +50,27 @@ Curl::Curl()
     }
 
     std::string header_auth = "Authorization: Bearer " + api_key;
-    this->headers = ::curl_slist_append(this->headers, header_auth.c_str());
+    this->headers = curl_slist_append(this->headers, header_auth.c_str());
 
-    if (not ::configs.project_id.empty())
+    if (not configs.project_id.empty())
     {
-        std::string header_project_id = "OpenAI-Project: " + ::configs.project_id;
-        this->headers = ::curl_slist_append(this->headers, header_project_id.c_str());
+        std::string header_project_id = "OpenAI-Project: " + configs.project_id;
+        this->headers = curl_slist_append(this->headers, header_project_id.c_str());
     }
 
-    this->headers = ::curl_slist_append(this->headers, "Content-Type: application/json");
-    ::curl_easy_setopt(this->handle, ::CURLOPT_HTTPHEADER, this->headers);
+    this->headers = curl_slist_append(this->headers, "Content-Type: application/json");
+    curl_easy_setopt(this->handle, CURLOPT_HTTPHEADER, this->headers);
 
-    ::curl_easy_setopt(this->handle, ::CURLOPT_WRITEFUNCTION, ::write_callback);
+    curl_easy_setopt(this->handle, CURLOPT_WRITEFUNCTION, write_callback);
 }
 
 Curl::~Curl()
 {
     if (this->handle)
     {
-        ::curl_slist_free_all(this->headers);
-        ::curl_easy_cleanup(this->handle);
+        curl_slist_free_all(this->headers);
+        curl_easy_cleanup(this->handle);
     }
 
-    ::curl_global_cleanup();
+    curl_global_cleanup();
 }
