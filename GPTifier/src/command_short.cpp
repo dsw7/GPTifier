ddf6aba2f00e7acbccc40ad8e524ed6380e8032c
dsw7 <dds3443@hotmail.com>
2024-09-25 23:51:49 -0700

    Basic compilable example

--- GPTifier/src/command_short.cpp
+++ GPTifier/src/command_short.cpp
@@ -1,5 +1,11 @@
 #include "command_short.hpp"
 
+#include "cli.hpp"
+#include <iostream>
+#include <string>
+
 void command_short(const int argc, char **argv)
 {
+    std::string prompt = cli::get_opts_short(argc, argv);
+    std::cout << prompt;
 }
