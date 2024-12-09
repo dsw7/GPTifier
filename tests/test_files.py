590c0573eea1bd3113fc9ed26970fab782fc1778
dsw7 <dds3443@hotmail.com>
2024-12-09 02:09:12 -0800

    Add help test for list subcommand

--- tests/test_files.py
+++ tests/test_files.py
@@ -14,6 +14,19 @@ def test_files_help(command: Command, option: str, capfd: Capture) -> None:
     assert "SYNOPSIS" in stdout
 
 
+@mark.parametrize("option", ["-h", "--help"])
+@mark.parametrize("subcommand", ["list"])
+def test_files_subcommand_help(
+    command: Command, subcommand: str, option: str, capfd: Capture
+) -> None:
+    command.extend(["files", subcommand, option])
+    process = run(command)
+
+    stdout, _ = unpack_stdout_stderr(capfd)
+    assert process.returncode == EX_OK
+    assert "SYNOPSIS" in stdout
+
+
 def test_files_list(command: Command, capfd: Capture) -> None:
     command.extend(["files", "list"])
     process = run(command)
