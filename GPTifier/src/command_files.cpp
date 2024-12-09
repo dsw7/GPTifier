2a6af2ee51f3c2e816b88717de7767dd15a10b52
dsw7 <dds3443@hotmail.com>
2024-12-09 01:29:01 -0800

    Hit list endpoint

--- GPTifier/src/command_files.cpp
+++ GPTifier/src/command_files.cpp
@@ -3,15 +3,60 @@
 #include "api.hpp"
 #include "cli.hpp"
 #include "help_messages.hpp"
+#include "json.hpp"
+#include "reporting.hpp"
+#include "utils.hpp"
 
+#include <fmt/core.h>
 #include <iostream>
+#include <map>
 #include <string>
 
 namespace {
 
+struct File {
+    std::string filename;
+    std::string id;
+    std::string purpose;
+};
+
+void print_file(int created_at, const File &file)
+{
+    const std::string datetime = datetime_from_unix_timestamp(created_at);
+    fmt::print("{:<30}{:<30}{:<30}{}\n", file.id, file.filename, datetime, file.purpose);
+}
+
 void command_files_list()
 {
-    std::cout << query_list_files_api();
+    const std::string response = query_list_files_api();
+
+    nlohmann::json results = nlohmann::json::parse(response);
+
+    if (results.contains("error")) {
+        const std::string error = results["error"]["message"];
+        reporting::print_error(error);
+        return;
+    }
+
+    reporting::print_sep();
+    fmt::print("{:<30}{:<30}{:<30}{}\n", "File ID", "Filename", "Creation time", "Purpose");
+
+    reporting::print_sep();
+    std::map<int, File> files = {};
+
+    for (const auto &entry: results["data"]) {
+        File file;
+        file.id = entry["id"];
+        file.filename = entry["filename"];
+        file.purpose = entry["purpose"];
+        files[entry["created_at"]] = file;
+    }
+
+    for (auto it = files.begin(); it != files.end(); ++it) {
+        print_file(it->first, it->second);
+    }
+
+    reporting::print_sep();
 }
 
 } // namespace
