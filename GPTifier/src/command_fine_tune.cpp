bbb9e6d11919fadd9474b7b447859f0a4b43d021
dsw7 <dds3443@hotmail.com>
2024-12-20 03:33:54 -0800

    Add function for querying upload API

--- GPTifier/src/command_fine_tune.cpp
+++ GPTifier/src/command_fine_tune.cpp
@@ -1,7 +1,46 @@
 #include "command_fine_tune.hpp"
 
+#include "api.hpp"
 #include "cli.hpp"
 #include "help_messages.hpp"
+#include "json.hpp"
+#include "reporting.hpp"
+
+#include <fmt/core.h>
+#include <string>
+
+namespace {
+
+void upload_fine_tuning_file(int argc, char **argv)
+{
+    if (argc < 4) {
+        cli::help_command_files_upload();
+        return;
+    }
+
+    const std::string opt_or_filename = argv[3];
+
+    if (opt_or_filename == "-h" or opt_or_filename == "--help") {
+        cli::help_command_files_upload();
+        return;
+    }
+
+    const std::string response = query_upload_file_api(opt_or_filename);
+    nlohmann::json results = nlohmann::json::parse(response);
+
+    if (results.contains("error")) {
+        const std::string error = results["error"]["message"];
+        reporting::print_error(error);
+        return;
+    }
+
+    const std::string filename = results["filename"];
+    const std::string id = results["id"];
+
+    fmt::print("Success!\nUploaded file: {}\nWith ID: {}\n", filename, id);
+}
+
+} // namespace
 
 void command_fine_tune(int argc, char **argv)
 {
@@ -15,5 +54,12 @@ void command_fine_tune(int argc, char **argv)
     if (subcommand == "-h" or subcommand == "--help") {
         cli::help_command_fine_tune();
         exit(EXIT_SUCCESS);
+    }
+
+    if (subcommand == "upload-file") {
+        upload_fine_tuning_file(argc, argv);
+    } else {
+        cli::help_command_fine_tune();
+        exit(EXIT_FAILURE);
     }
 }
