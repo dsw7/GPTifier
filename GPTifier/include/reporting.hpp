8c7c04f409c77f934d8c3a244dcd49ed77b4fddc
dsw7 <dds3443@hotmail.com>
2024-12-21 03:09:24 -0800

    Remove unused function

--- GPTifier/include/reporting.hpp
+++ GPTifier/include/reporting.hpp
@@ -6,7 +6,6 @@ namespace reporting {
 
 void print_sep();
 void print_error(const std::string &message);
-void print_request(const std::string &request);
 void print_response(const std::string &response);
 void print_results(const std::string &results);
 
