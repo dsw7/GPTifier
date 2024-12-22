d3f130635afae44e8b2f42c35cdeaaf34cdb84dd
dsw7 <dds3443@hotmail.com>
2024-12-22 03:07:58 -0800

    Catch low level parsing errors in dedicated function

--- GPTifier/src/parsers.cpp
+++ GPTifier/src/parsers.cpp
@@ -2,9 +2,32 @@
 
 #include "reporting.hpp"
 
+#include <fmt/core.h>
+#include <iostream>
+#include <stdexcept>
+
+namespace {
+
+nlohmann::json catch_deserialization_errors(const std::string &response)
+{
+    // Throw an exception for low level errors
+    nlohmann::json results;
+
+    try {
+        results = nlohmann::json::parse(response);
+    } catch (const nlohmann::json::parse_error &e) {
+        const std::string errmsg = fmt::format("Failed to parse completion. Error was:\n{}", e.what());
+        throw std::runtime_error(errmsg);
+    }
+
+    return results;
+}
+
+} // namespace
+
 std::optional<nlohmann::json> parse_response(const std::string &response)
 {
-    nlohmann::json results = nlohmann::json::parse(response);
+    nlohmann::json results = catch_deserialization_errors(response);
 
     if (not results.contains("error")) {
         return results;
