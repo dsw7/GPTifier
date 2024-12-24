efac29a9de962b8dc1acb64bb215e33d42fc4832
dsw7 <dds3443@hotmail.com>
2024-12-24 04:03:44 -0800

    Replace heterogeneity

--- GPTifier/src/command_models.cpp
+++ GPTifier/src/command_models.cpp
@@ -19,18 +19,12 @@ struct OpenAIModel {
     std::string owned_by;
 };
 
-struct UserModel {
-    int creation_time;
-    std::string id;
-    std::string owned_by;
-};
-
 bool is_fine_tuning_model(const std::string &model)
 {
     return model.compare(0, 3, "ft:") == 0;
 }
 
-void print_openai_models(const std::map<int, OpenAIModel> &models)
+void print_models(const std::map<int, OpenAIModel> &models)
 {
     reporting::print_sep();
     fmt::print("{:<25}{:<35}{}\n", "Creation time", "Owner", "Model ID");
@@ -44,34 +38,19 @@ void print_openai_models(const std::map<int, OpenAIModel> &models)
     reporting::print_sep();
 }
 
-void print_user_models(const std::vector<UserModel> &models)
-{
-    reporting::print_sep();
-    fmt::print("{:<25}{:<35}{}\n", "Creation time", "Owner", "Model ID");
-    reporting::print_sep();
-
-    for (auto it = models.begin(); it != models.end(); ++it) {
-        const std::string datetime = datetime_from_unix_timestamp(it->creation_time);
-        fmt::print("{:<25}{:<35}{}\n", datetime, it->owned_by, it->id);
-    }
-
-    reporting::print_sep();
-}
-
 void print_models_response(const std::string &response)
 {
     const nlohmann::json results = parse_response(response);
 
-    std::vector<UserModel> user_models = {};
     std::map<int, OpenAIModel> openai_models = {};
+    std::map<int, OpenAIModel> user_models = {};
 
     for (const auto &entry: results["data"]) {
         if (is_fine_tuning_model(entry["id"])) {
-            UserModel model;
-            model.creation_time = entry["created"];
+            OpenAIModel model;
             model.id = entry["id"];
             model.owned_by = entry["owned_by"];
-            user_models.push_back(model);
+            user_models[entry["created"]] = model;
         } else {
             OpenAIModel model;
             model.id = entry["id"];
@@ -81,11 +60,11 @@ void print_models_response(const std::string &response)
     }
 
     fmt::print("> OpenAI models:\n");
-    print_openai_models(openai_models);
+    print_models(openai_models);
 
     if (not user_models.empty()) {
         fmt::print("\n> User models:\n");
-        print_user_models(user_models);
+        print_models(user_models);
     }
 }
 
