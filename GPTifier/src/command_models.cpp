7c5c835fa3ec93d250894cdd0280a9877b769558
dsw7 <dds3443@hotmail.com>
2024-12-24 05:21:47 -0800

    Restore usage of struct

--- GPTifier/src/command_models.cpp
+++ GPTifier/src/command_models.cpp
@@ -10,13 +10,18 @@
 #include <algorithm>
 #include <fmt/core.h>
 #include <string>
-#include <tuple>
+#include <utility>
 #include <vector>
 
 namespace {
 
-typedef std::tuple<int, std::string, std::string> tup_model;
-typedef std::vector<tup_model> vec_models;
+struct OpenAIModel {
+    std::string id;
+    std::string owned_by;
+};
+
+typedef std::pair<int, OpenAIModel> pair_model;
+typedef std::vector<pair_model> vec_models;
 
 bool is_fine_tuning_model(const std::string &model)
 {
@@ -29,18 +34,17 @@ void print_models(vec_models &models)
     fmt::print("{:<25}{:<35}{}\n", "Creation time", "Owner", "Model ID");
     reporting::print_sep();
 
-    std::sort(models.begin(), models.end(), [](const tup_model &a, const tup_model &b) {
-        if (std::get<0>(a) != std::get<0>(b)) {
-            return std::get<0>(a) < std::get<0>(b);
+    std::sort(models.begin(), models.end(), [](const pair_model &a, const pair_model &b) {
+        if (a.first != b.first) {
+            return a.first < b.first;
         }
 
-        // otherwise sort by model ID
-        return std::get<2>(a) < std::get<2>(b);
+        return a.second.id < b.second.id;
     });
 
     for (auto it = models.begin(); it != models.end(); ++it) {
-        const std::string datetime = datetime_from_unix_timestamp(std::get<0>(*it));
-        fmt::print("{:<25}{:<35}{}\n", datetime, std::get<1>(*it), std::get<2>(*it));
+        const std::string datetime = datetime_from_unix_timestamp(it->first);
+        fmt::print("{:<25}{:<35}{}\n", datetime, it->second.owned_by, it->second.id);
     }
 
     reporting::print_sep();
@@ -55,9 +59,15 @@ void print_models_response(const std::string &response)
 
     for (const auto &entry: results["data"]) {
         if (is_fine_tuning_model(entry["id"])) {
-            user_models.push_back({ entry["created"], entry["owned_by"], entry["id"] });
+            OpenAIModel model;
+            model.id = entry["id"];
+            model.owned_by = entry["owned_by"];
+            user_models.push_back(std::make_pair(entry["created"], model));
         } else {
-            openai_models.push_back({ entry["created"], entry["owned_by"], entry["id"] });
+            OpenAIModel model;
+            model.id = entry["id"];
+            model.owned_by = entry["owned_by"];
+            openai_models.push_back(std::make_pair(entry["created"], model));
         }
     }
 
