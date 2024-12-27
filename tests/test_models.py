46e452d6b85f66f48d366468c12abcae204bceec
dsw7 <dds3443@hotmail.com>
2024-12-27 04:39:42 -0800

    Add raw tests for models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,35 @@
 from os import EX_OK
 from subprocess import run
 from pytest import mark
-from utils import unpack_stdout_stderr, Command, Capture
+import utils
 
 
 @mark.parametrize("option", ["-h", "--help"])
-def test_models_help(command: Command, option: str, capfd: Capture) -> None:
+def test_models_help(command: utils.Command, option: str, capfd: utils.Capture) -> None:
     command.extend(["models", option])
     process = run(command)
 
-    stdout, _ = unpack_stdout_stderr(capfd)
+    stdout, _ = utils.unpack_stdout_stderr(capfd)
     assert process.returncode == EX_OK
     assert "Synopsis" in stdout
 
 
-def test_models(command: Command, capfd: Capture) -> None:
+def test_models(command: utils.Command, capfd: utils.Capture) -> None:
     command.extend(["models"])
     process = run(command)
 
-    stdout, _ = unpack_stdout_stderr(capfd)
+    stdout, _ = utils.unpack_stdout_stderr(capfd)
     assert process.returncode == EX_OK
 
     models = stdout.split("\n")
     assert len(models) > 1
+
+
+@mark.parametrize("option", ["-r", "--raw"])
+def test_models_raw(command: utils.Command, option: str, capfd: utils.Capture) -> None:
+    command.extend(["models", option])
+    process = run(command)
+
+    stdout, _ = utils.unpack_stdout_stderr(capfd)
+    assert process.returncode == EX_OK
+    utils.assert_valid_json(stdout)
