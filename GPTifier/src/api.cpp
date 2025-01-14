443b8a6c693128565c5d185e8f634fb778fd3d8d
dsw7 <dds3443@hotmail.com>
2025-01-14 03:32:01 -0800

    Omit automatic copy constructor and assignment operator

--- GPTifier/src/api.cpp
+++ GPTifier/src/api.cpp
@@ -49,6 +49,13 @@ class Curl {
 public:
     Curl();
     ~Curl();
+
+    // We want to prevent any copies from being made otherwise we'll attempt
+    // to delete a shallow copy of the headers list multiple times (i.e. because the destructor will
+    // be called for each copy)
+    Curl(const Curl &) = delete;
+    Curl &operator=(const Curl &) = delete;
+
     std::string get_uploaded_files();
     std::string get_models();
     std::string create_chat_completion(const std::string &post_fields);
