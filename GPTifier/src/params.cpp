692cecba1f6ac0d2cd8440adb77918ecaa967505
dsw7 <dds3443@hotmail.com>
2025-01-29 01:35:57 -0800

    Add the client side check

--- GPTifier/src/params.cpp
+++ GPTifier/src/params.cpp
@@ -3,6 +3,19 @@
 #include <fmt/core.h>
 #include <stdexcept>
 
+namespace {
+
+bool is_valid_temp(float temperature)
+{
+    if (temperature < 0 || temperature > 2) {
+        return false;
+    }
+
+    return true;
+}
+
+} // namespace
+
 void ParamsRun::sanitize()
 {
     if (std::holds_alternative<float>(this->temperature)) {
@@ -16,5 +29,11 @@ void ParamsRun::sanitize()
     } catch (std::invalid_argument &e) {
         const std::string errmsg = fmt::format("{}\nFailed to convert '{}' to float", e.what(), temperature);
         throw std::runtime_error(errmsg);
+    }
+
+    if (std::holds_alternative<float>(this->temperature)) {
+        if (not is_valid_temp(std::get<float>(this->temperature))) {
+            throw std::runtime_error("Temperature must be between 0 and 2");
+        }
     }
 }
