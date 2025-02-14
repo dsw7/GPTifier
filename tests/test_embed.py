6dc18722172f2de26a22248897340c8f4b895901
dsw7 <dds3443@hotmail.com>
2025-02-14 01:54:10 -0800

    Add orthogonal embeddings test

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from json import loads
 from pathlib import Path
+from tempfile import gettempdir
 from unittest import TestCase
 from .helpers import run_process
 
@@ -12,10 +13,8 @@ class Embedding:
     text: str
 
 
-def load_embedding() -> Embedding:
-    results = Path.home() / ".gptifier" / "embeddings.gpt"
-
-    with results.open() as f:
+def load_embedding(results_file: Path) -> Embedding:
+    with results_file.open() as f:
         data = loads(f.read())
 
     return Embedding(
@@ -23,64 +22,95 @@ def load_embedding() -> Embedding:
     )
 
 
-class TestEmbed(TestCase):
+def get_cosine_similarity(left: Embedding, right: Embedding) -> float:
+    dot_p: float = 0
 
-    def test_help(self) -> None:
-        for option in ["-h", "--help"]:
-            with self.subTest(option=option):
-                proc = run_process(["embed", option])
+    for l, r in zip(left.embedding, right.embedding):
+        dot_p += l * r
 
-                proc.assert_success()
-                self.assertIn("Synopsis", proc.stdout)
+    mag_l: float = (sum(i**2 for i in left.embedding)) ** 0.5
+    mag_r: float = (sum(i**2 for i in right.embedding)) ** 0.5
 
-    def test_basic(self) -> None:
-        text = "What is 3 + 5?"
-        model = "text-embedding-ada-002"
-
-        proc = run_process(["embed", f"-i{text}", f"-m{model}"])
-        proc.assert_success()
-
-        results = load_embedding()
-        self.assertEqual(results.model, model)
-        self.assertEqual(results.text, text)
-        self.assertEqual(
-            len(results.embedding), 1536
-        )  # text-embedding-ada-002 dimension
-
-    def test_read_from_file(self) -> None:
-        input_text_file = Path(__file__).resolve().parent / "prompt_basic.txt"
-        model = "text-embedding-3-small"
-
-        proc = run_process(["embed", f"-r{input_text_file}", f"-m{model}"])
-        proc.assert_success()
-
-        results = load_embedding()
-        self.assertEqual(results.model, model)
-        self.assertEqual(results.text, input_text_file.read_text())
-
-    def test_missing_input_file(self) -> None:
-        proc = run_process(["embed", "--read-from-file=/tmp/yU8nnkRs.txt"])
-        proc.assert_failure()
-        self.assertIn("Could not open file '/tmp/yU8nnkRs.txt'", proc.stderr)
-
-    def test_invalid_model(self) -> None:
-        proc = run_process(["embed", "-i'What is 3 + 5?'", "-mfoobar"])
-        proc.assert_failure()
-        self.assertIn(
-            "The model `foobar` does not exist or you do not have access to it.",
-            proc.stderr,
-        )
+    return dot_p / (mag_l * mag_r)
 
 
-class TestEmbedReadFromInputfile(TestCase):
+class TestEmbedCosineSimilarityOrthogonal(TestCase):
+
     def setUp(self) -> None:
-        self.filename = Path.cwd() / "Inputfile"
-        self.filename.write_text("A foo that bars!")
+        self.filename_1 = Path(gettempdir()) / "result_1.gpt"
+        self.filename_2 = Path(gettempdir()) / "result_2.gpt"
 
     def tearDown(self) -> None:
-        self.filename.unlink()
+        if self.filename_1.exists():
+            self.filename_1.unlink()
 
-    def test_read_from_inputfile(self) -> None:
-        proc = run_process("embed")
-        proc.assert_success()
-        self.assertIn("Found an Inputfile in current working directory!", proc.stdout)
+        if self.filename_2.exists():
+            self.filename_2.unlink()
+
+    def test_compute_cosine_similarities(self) -> None:
+        model = "text-embedding-ada-002"
+
+        text_1 = "The cat meowed softly."
+        proc_1 = run_process(
+            ["embed", f"-i{text_1}", f"-m{model}", f"-o{self.filename_1}"]
+        )
+        proc_1.assert_success()
+        embedding_1 = load_embedding(self.filename_1)
+
+        text_2 = "Quantum physics is fascinating."
+        proc_2 = run_process(
+            ["embed", f"-i{text_2}", f"-m{model}", f"-o{self.filename_2}"]
+        )
+        proc_2.assert_success()
+        embedding_2 = load_embedding(self.filename_2)
+
+        self.assertTrue(0.6 <= get_cosine_similarity(embedding_1, embedding_2) <= 0.8)
+
+
+# class TestEmbed(TestCase):
+#
+#    def test_help(self) -> None:
+#        for option in ["-h", "--help"]:
+#            with self.subTest(option=option):
+#                proc = run_process(["embed", option])
+#
+#                proc.assert_success()
+#                self.assertIn("Synopsis", proc.stdout)
+#
+#    def test_basic(self) -> None:
+#        text = "What is 3 + 5?"
+#        model = "text-embedding-ada-002"
+#
+#        proc = run_process(["embed", f"-i{text}", f"-m{model}"])
+#        proc.assert_success()
+#
+#        results = load_embedding()
+#        self.assertEqual(results.model, model)
+#        self.assertEqual(results.text, text)
+#        self.assertEqual(
+#            len(results.embedding), 1536
+#        )  # text-embedding-ada-002 dimension
+#
+#    def test_read_from_file(self) -> None:
+#        input_text_file = Path(__file__).resolve().parent / "prompt_basic.txt"
+#        model = "text-embedding-3-small"
+#
+#        proc = run_process(["embed", f"-r{input_text_file}", f"-m{model}"])
+#        proc.assert_success()
+#
+#        results = load_embedding()
+#        self.assertEqual(results.model, model)
+#        self.assertEqual(results.text, input_text_file.read_text())
+#
+#    def test_missing_input_file(self) -> None:
+#        proc = run_process(["embed", "--read-from-file=/tmp/yU8nnkRs.txt"])
+#        proc.assert_failure()
+#        self.assertIn("Could not open file '/tmp/yU8nnkRs.txt'", proc.stderr)
+#
+#    def test_invalid_model(self) -> None:
+#        proc = run_process(["embed", "-i'What is 3 + 5?'", "-mfoobar"])
+#        proc.assert_failure()
+#        self.assertIn(
+#            "The model `foobar` does not exist or you do not have access to it.",
+#            proc.stderr,
+#        )
