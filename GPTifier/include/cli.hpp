0543960149fa0e6abcb11e3e8140f8f22986b83c
dsw7 <dds3443@hotmail.com>
2025-02-15 01:35:51 -0800

    Update cli to handle --user flag

--- GPTifier/include/cli.hpp
+++ GPTifier/include/cli.hpp
@@ -6,12 +6,12 @@ namespace cli {
 
 ParamsRun get_opts_run(int argc, char **argv);
 ParamsShort get_opts_short(int argc, char **argv);
+ParamsModels get_opts_models(int argc, char **argv);
 ParamsEmbedding get_opts_embed(int argc, char **argv);
 ParamsFineTune get_opts_create_fine_tuning_job(int argc, char **argv);
 ParamsGetFineTuningJobs get_opts_get_fine_tuning_jobs(int argc, char **argv);
 ParamsCosts get_opts_get_costs(int argc, char **argv);
 
-bool get_opts_models(int argc, char **argv);
 bool get_opts_files_list(int argc, char **argv);
 
 } // namespace cli
