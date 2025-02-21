3b8d214743a2afcdba8f4adba9f791bf55281c02
dsw7 <dds3443@hotmail.com>
2025-02-21 01:48:18 -0800

    Add yellow to color palette

--- GPTifier/include/utils.hpp
+++ GPTifier/include/utils.hpp
@@ -8,6 +8,7 @@ constexpr fmt::terminal_color blue = fmt::terminal_color::bright_blue;
 constexpr fmt::terminal_color green = fmt::terminal_color::bright_green;
 constexpr fmt::terminal_color red = fmt::terminal_color::bright_red;
 constexpr fmt::terminal_color white = fmt::terminal_color::bright_white;
+constexpr fmt::terminal_color yellow = fmt::terminal_color::bright_yellow;
 
 void str_to_lowercase(std::string &str);
 void print_sep();
