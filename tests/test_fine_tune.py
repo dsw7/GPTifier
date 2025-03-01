bd7d0004e0cbd431bea9220475c5e9e9a72591d4
dsw7 <dds3443@hotmail.com>
2025-02-28 23:11:16 -0800

    Separate concerns in FT test

--- tests/test_fine_tune.py
+++ tests/test_fine_tune.py
@@ -4,21 +4,20 @@ from .helpers import run_process
 
 
 class TestFineTune(TestCase):
-
     def test_help(self) -> None:
         for option in ["-h", "--help"]:
             with self.subTest(option=option):
                 proc = run_process(["fine-tune", option])
                 proc.assert_success()
-                self.assertIn("Synopsis", proc.stdout)
+                self.assertIn("Manage fine tuning operations.", proc.stdout)
 
-    def test_subcommand_help(self) -> None:
-        for subcommand in ["upload-file", "create-job", "delete-model", "list-jobs"]:
-            for option in ["-h", "--help"]:
-                with self.subTest(subcommand=subcommand, option=option):
-                    proc = run_process(["fine-tune", subcommand, option])
-                    proc.assert_success()
-                    self.assertIn("Synopsis", proc.stdout)
+
+class TestFineTuneUploadFile(TestCase):
+    def test_help(self) -> None:
+        for option in ["-h", "--help"]:
+            proc = run_process(["fine-tune", "upload-file", option])
+            proc.assert_success()
+            self.assertIn("Upload a fine-tuning file.", proc.stdout)
 
     def test_upload_missing_file(self) -> None:
         proc = run_process(["fine-tune", "upload-file", "foobar"])
@@ -35,6 +34,14 @@ class TestFineTune(TestCase):
             "Invalid file format for Fine-Tuning API. Must be .jsonl", proc.stderr
         )
 
+
+class TestFineTuneCreateJob(TestCase):
+    def test_help(self) -> None:
+        for option in ["-h", "--help"]:
+            proc = run_process(["fine-tune", "create-job", option])
+            proc.assert_success()
+            self.assertIn("Create a fine-tuning job.", proc.stdout)
+
     def test_create_job_invalid_params(self) -> None:
         process = run_process(
             ["fine-tune", "create-job", "--model=foobar", "--file-id=foobar"]
@@ -42,10 +49,26 @@ class TestFineTune(TestCase):
         process.assert_failure()
         self.assertIn("invalid training_file: foobar", process.stderr)
 
+
+class TestFineTuneDeleteModel(TestCase):
+    def test_help(self) -> None:
+        for option in ["-h", "--help"]:
+            proc = run_process(["fine-tune", "delete-model", option])
+            proc.assert_success()
+            self.assertIn("Delete a fine-tuned model.", proc.stdout)
+
     def test_delete_model_missing_model(self) -> None:
         process = run_process(["fine-tune", "delete-model", "foobar"])
         process.assert_failure()
         self.assertIn("The model 'foobar' does not exist", process.stderr)
+
+
+class TestFineTuneListJobs(TestCase):
+    def test_help(self) -> None:
+        for option in ["-h", "--help"]:
+            proc = run_process(["fine-tune", "list-jobs", option])
+            proc.assert_success()
+            self.assertIn("List fine-tuning jobs.", proc.stdout)
 
     def test_list_jobs_raw_json(self) -> None:
         for option in ["-j", "--json"]:
