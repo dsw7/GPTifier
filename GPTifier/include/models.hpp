92659beee391ac017e44664960ff8571dac9b3a4
dsw7 <dds3443@hotmail.com>
2025-03-25 00:26:40 -0700

    Remove models src

--- GPTifier/include/models.hpp
+++ GPTifier/include/models.hpp
@@ -1,32 +1,8 @@
 #pragma once
 
-#include <chrono>
-#include <ctime>
-#include <json.hpp>
 #include <string>
-#include <vector>
 
 namespace models {
-
-struct ChatCompletion {
-    int completion_tokens = 0;
-    int prompt_tokens = 0;
-    std::chrono::duration<float> rtt;
-    std::string completion;
-    std::string id;
-    std::string model;
-    std::string prompt;
-    std::time_t created = 0;
-
-    nlohmann::json jsonify() const;
-};
-
-struct File {
-    int created_at;
-    std::string filename;
-    std::string id;
-    std::string purpose;
-};
 
 struct FineTuningJob {
     int created_at;
