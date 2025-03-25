44cb6e232dca358e332228d78e254b38a517aedb
dsw7 <dds3443@hotmail.com>
2025-03-25 02:46:31 -0700

    Remove using directives

--- GPTifier/src/serialization/embeddings.cpp
+++ GPTifier/src/serialization/embeddings.cpp
@@ -4,23 +4,23 @@
 #include "serialization/parse_response.hpp"
 #include "serialization/validation.hpp"
 
-using json = nlohmann::json;
+#include <json.hpp>
 
 Embedding create_embedding(const std::string &model, const std::string &input)
 {
-    const json data = { { "model", model }, { "input", input } };
+    const nlohmann::json data = { { "model", model }, { "input", input } };
 
     OpenAIUser api;
     const std::string response = api.create_embedding(data.dump());
+    const nlohmann::json results = parse_response(response);
 
-    const json results = parse_response(response);
     validation::is_list(results);
 
     if (validation::is_list_empty(results)) {
         throw std::runtime_error("List of embeddings from OpenAI is empty");
     }
 
-    const json first_embedding = results["data"][0];
+    const nlohmann::json first_embedding = results["data"][0];
     validation::is_embedding(first_embedding);
 
     Embedding em;
--- GPTifier/src/serialization/parse_response.cpp
+++ GPTifier/src/serialization/parse_response.cpp
@@ -3,21 +3,19 @@
 #include <fmt/core.h>
 #include <stdexcept>
 
-using json = nlohmann::json;
-
 namespace {
 
-void catch_deserialization_errors(json &results, const std::string &response)
+void catch_deserialization_errors(nlohmann::json &results, const std::string &response)
 {
     try {
-        results = json::parse(response);
-    } catch (const json::parse_error &e) {
+        results = nlohmann::json::parse(response);
+    } catch (const nlohmann::json::parse_error &e) {
         const std::string errmsg = fmt::format("Failed to parse completion. Error was:\n{}", e.what());
         throw std::runtime_error(errmsg);
     }
 }
 
-void catch_errors_from_openai(const json &results)
+void catch_errors_from_openai(const nlohmann::json &results)
 {
     if (not results.contains("error")) {
         return;
@@ -33,9 +31,9 @@ void catch_errors_from_openai(const json &results)
 
 } // namespace
 
-json parse_response(const std::string &response)
+nlohmann::json parse_response(const std::string &response)
 {
-    json results;
+    nlohmann::json results;
     catch_deserialization_errors(results, response);
     catch_errors_from_openai(results);
 
