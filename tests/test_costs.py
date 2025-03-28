ec81c539be7fc635500fdb36936645dc757daa50
dsw7 <dds3443@hotmail.com>
2025-03-28 02:19:58 -0700

    Simplify test_costs

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,36 +1,29 @@
-import unittest
+from unittest import skipIf
 from os import environ
-from .helpers import run_process
+from .extended_testcase import TestCaseExtended
 
 
-@unittest.skipIf(
+@skipIf(
     "OPENAI_ADMIN_KEY" not in environ, "OPENAI_ADMIN_KEY does not exist in environment"
 )
-class TestCosts(unittest.TestCase):
+class TestCosts(TestCaseExtended):
 
     def test_help(self) -> None:
         for option in ["-h", "--help"]:
             with self.subTest(option=option):
-                proc = run_process(["costs", option])
-
-                proc.assert_success()
+                proc = self.assertSuccess("costs", option)
                 self.assertIn("Get OpenAI usage details.", proc.stdout)
 
     def test_costs(self) -> None:
-        proc = run_process(["costs", "--days=4"])
-        proc.assert_success()
-
-        costs = proc.stdout.split("\n")
-        assert len(costs) > 1
+        proc = self.assertSuccess("costs", "--days=4")
+        self.assertIn("Overall usage", proc.stdout)
 
     def test_costs_raw_json(self) -> None:
         for option in ["-j", "--json"]:
             with self.subTest(option=option):
-                proc = run_process(["costs", option, "--days=3"])
-                proc.assert_success()
+                proc = self.assertSuccess("costs", option, "--days=3")
                 proc.load_stdout_to_json()
 
     def test_costs_invalid_days(self) -> None:
-        proc = run_process(["costs", "--days=-1"])
-        proc.assert_failure()
+        proc = self.assertFailure("costs", "--days=-1")
         self.assertIn("Days must be greater than 0", proc.stderr)
