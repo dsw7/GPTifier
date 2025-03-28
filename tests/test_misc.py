f82869f787b42d6c43b33aa5264174291b284876
dsw7 <dds3443@hotmail.com>
2025-03-28 03:39:52 -0700

    Clean up misc tests

---
+++ tests/test_misc.py
@@ -0,0 +1,20 @@
+from .extended_testcase import TestCaseExtended
+
+
+class TestCatchMemoryLeak(TestCaseExtended):
+
+    def test_catch_memory_leak(self) -> None:
+        proc = self.assertSuccess("test", "leak")
+        self.assertEqual(proc.stdout.strip(), "5")
+
+
+class TestCurlHandleReusability(TestCaseExtended):
+
+    def test_reuse_curl_handle(self) -> None:
+        # Test that curl handle can be reused in GPTifier's network layer
+
+        proc = self.assertSuccess("test", "ccc")
+        self.assertDictEqual(
+            proc.load_stdout_to_json(),
+            {"result_1": ">>>10<<<", "result_2": ">>>10<<<", "result_3": ">>>10<<<"},
+        )
