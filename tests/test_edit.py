026ce0cc88f1366fa49249503b7e65f37b2f5522
dsw7 <dds3443@hotmail.com>
2025-03-29 04:09:15 -0700

    Add test for writing to stdout

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,4 +1,35 @@
+from pathlib import Path
 from .extended_testcase import TestCaseExtended
+
+
+INPUT_CODE = (
+    "from .myfunc import myfunc\n"
+    "\n"
+    "def main() -> None:\n"
+    '    myfunc(["a", "b", "c"])\n'
+    "\n"
+    'if __name__ == "__main__":\n'
+    "    main()\n"
+)
+PROMPT = (
+    "Given we have a function call of form:\n"
+    "```\n"
+    "proc = myfunc([A, B, C])\n"
+    "```\n"
+    "Replace this call with a call of form:\n"
+    "```\n"
+    "proc = myfunc(A, B, C)\n"
+    "```\n"
+)
+OUTPUT_CODE = (
+    "from .myfunc import myfunc\n"
+    "\n"
+    "def main() -> None:\n"
+    '    myfunc("a", "b", "c")\n'
+    "\n"
+    'if __name__ == "__main__":\n'
+    "    main()\n"
+)
 
 
 class TestEdit(TestCaseExtended):
@@ -18,3 +49,20 @@ class TestEdit(TestCaseExtended):
     def test_missing_edit_file(self) -> None:
         proc = self.assertFailure("edit", "prompt")
         self.assertIn("No file to edit provided. Cannot proceed", proc.stderr)
+
+
+class TestEditFile(TestCaseExtended):
+
+    def setUp(self) -> None:
+        self.input_file = Path("/tmp/foo.py")
+        self.prompt_file = Path("/tmp/prompt")
+        self.input_file.write_text(INPUT_CODE)
+        self.prompt_file.write_text(PROMPT)
+
+    def tearDown(self) -> None:
+        self.input_file.unlink()
+        self.prompt_file.unlink()
+
+    def test_write_to_stdout(self) -> None:
+        proc = self.assertSuccess("edit", str(self.prompt_file), str(self.input_file))
+        self.assertEqual(proc.stdout, OUTPUT_CODE)
