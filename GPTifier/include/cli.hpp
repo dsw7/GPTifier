6e38d5f3ac8932326a6b9942125dd8fdc2358ffb
dsw7 <dds3443@hotmail.com>
2025-03-28 23:04:15 -0700

    params files

--- GPTifier/include/cli.hpp
+++ GPTifier/include/cli.hpp
@@ -3,6 +3,5 @@
 namespace cli {
 
 void exit_on_failure();
-bool get_opts_files_list(int argc, char **argv);
 
 } // namespace cli
--- GPTifier/src/cli.cpp
+++ GPTifier/src/cli.cpp
@@ -1,10 +1,6 @@
 #include "cli.hpp"
 
-#include "help_messages.hpp"
-
 #include <fmt/core.h>
-#include <getopt.h>
-#include <string.h>
 
 namespace cli {
 
@@ -12,39 +8,6 @@ void exit_on_failure()
 {
     fmt::print(stderr, "Try running with -h or --help for more information\n");
     exit(EXIT_FAILURE);
-}
-
-bool get_opts_files_list(int argc, char **argv)
-{
-    bool print_raw_json = false;
-
-    while (true) {
-        static struct option long_options[] = {
-            { "help", no_argument, 0, 'h' },
-            { "json", no_argument, 0, 'j' },
-            { 0, 0, 0, 0 }
-        };
-
-        int option_index = 0;
-        int c = getopt_long(argc, argv, "hj", long_options, &option_index);
-
-        if (c == -1) {
-            break;
-        }
-
-        switch (c) {
-            case 'h':
-                help_command_files_list();
-                exit(EXIT_SUCCESS);
-            case 'j':
-                print_raw_json = true;
-                break;
-            default:
-                exit_on_failure();
-        }
-    }
-
-    return print_raw_json;
 }
 
 } // namespace cli
