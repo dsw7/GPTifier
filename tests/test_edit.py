dfe23a6bdfc65b45aecd8b4d33ab9e5aa51ff5b5
dsw7 <dds3443@hotmail.com>
2025-03-29 04:22:05 -0700

    Add test for writing to file

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -55,6 +55,7 @@ class TestEditFile(TestCaseExtended):
 
     def setUp(self) -> None:
         self.input_file = Path("/tmp/foo.py")
+        self.output_file = Path("/tmp/bar.py")
         self.prompt_file = Path("/tmp/prompt")
         self.input_file.write_text(INPUT_CODE)
         self.prompt_file.write_text(PROMPT)
@@ -63,6 +64,16 @@ class TestEditFile(TestCaseExtended):
         self.input_file.unlink()
         self.prompt_file.unlink()
 
+        if self.output_file.exists():
+            self.output_file.unlink()
+
     def test_write_to_stdout(self) -> None:
         proc = self.assertSuccess("edit", str(self.prompt_file), str(self.input_file))
-        self.assertEqual(proc.stdout, OUTPUT_CODE)
+        self.assertIn(proc.stdout, OUTPUT_CODE)
+
+    def test_write_to_file(self) -> None:
+        self.assertSuccess(
+            "edit", str(self.prompt_file), str(self.input_file), f"-o{self.output_file}"
+        )
+        self.assertTrue(self.output_file.exists())
+        self.assertIn(self.output_file.read_text(), OUTPUT_CODE)
