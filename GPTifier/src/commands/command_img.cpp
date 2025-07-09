857015eb11ad458162892860c2b51c840a32c1c7
dsw7 <dds3443@hotmail.com>
2025-07-09 01:02:07 -0700

    Add basic I/O

--- GPTifier/src/commands/command_img.cpp
+++ GPTifier/src/commands/command_img.cpp
@@ -1,8 +1,8 @@
 #include "command_img.hpp"
 
 #include "help_messages.hpp"
+#include "utils.hpp"
 
-#include <filesystem>
 #include <fmt/core.h>
 #include <getopt.h>
 #include <optional>
@@ -14,10 +14,11 @@ void help_img()
 {
     help::HelpMessages help;
     help.add_description("Generate an image from a prompt");
-    help.add_synopsis("img [OPTIONS] </path/to/prompt/file>");
+    help.add_synopsis("img [OPTIONS] PROMPT-FILE");
     help.add_option("-h", "--help", "Print help information and exit");
     help.add_option("-q", "--hd", "Request high definition image (default is standard)");
     help.add_option("-v", "--vivid", "Request hyper-realistic / dramatic image (default is natural)");
+    help.add_option("-o <filename>", "--output=<filename>", "Specify where to export image");
     help.print();
 }
 
@@ -26,7 +27,8 @@ struct Parameters {
     std::string quality = "standard";
     std::string size = "1024x1024";
     std::string style = "natural";
-    std::optional<std::filesystem::path> prompt_file;
+    std::optional<std::string> prompt_file;
+    std::optional<std::string> output_file;
 };
 
 Parameters read_cli(int argc, char **argv)
@@ -38,11 +40,12 @@ Parameters read_cli(int argc, char **argv)
             { "help", no_argument, 0, 'h' },
             { "hd", no_argument, 0, 'q' },
             { "vivid", no_argument, 0, 'v' },
+            { "output", required_argument, 0, 'o' },
             { 0, 0, 0, 0 },
         };
 
         int option_index = 0;
-        int c = getopt_long(argc, argv, "hqv", long_options, &option_index);
+        int c = getopt_long(argc, argv, "hqvo:", long_options, &option_index);
 
         if (c == -1) {
             break;
@@ -58,17 +61,19 @@ Parameters read_cli(int argc, char **argv)
             case 'v':
                 params.style = "vivid";
                 break;
+            case 'o':
+                params.output_file = optarg;
+                break;
             default:
                 help::exit_on_failure();
         }
     };
 
     for (int i = optind; i < argc; i++) {
-        params.prompt_file = argv[i];
-    }
-
-    if (not params.prompt_file) {
-        throw std::runtime_error("No prompt file provided. Cannot proceed");
+        if (strcmp("img", argv[i]) != 0) {
+            params.prompt_file = argv[i];
+            break;
+        }
     }
 
     return params;
@@ -86,7 +91,12 @@ void command_img(int argc, char **argv)
     }
 
     const Parameters params = read_cli(argc, argv);
-    fmt::print("{}\n", params.style);
+
+    if (not params.prompt_file) {
+        throw std::runtime_error("No prompt file provided. Cannot proceed");
+    }
+
+    const std::string prompt = utils::read_from_file(params.prompt_file.value());
 }
 
 } // namespace commands
