b3b9bd5ac1d781afe1cccca771e43c7d3608a3f9
dsw7 <dds3443@hotmail.com>
2025-09-05 20:12:28 -0700

    Remove get_chat_completions from networking layer

--- GPTifier/src/networking/api_openai_user.hpp
+++ GPTifier/src/networking/api_openai_user.hpp
@@ -6,7 +6,6 @@ namespace networking {
 std::string get_models();
 std::string delete_model(const std::string &model_id);
 std::string create_chat_completion(const std::string &post_fields);
-std::string get_chat_completions(int limit);
 std::string create_embedding(const std::string &post_fields);
 std::string upload_file(const std::string &filename, const std::string &purpose);
 std::string get_uploaded_files(bool sort_asc = true);
