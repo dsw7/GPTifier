a7f7ec7339664699febd2c5d8c7c41d5bf7653c4
dsw7 <dds3443@hotmail.com>
2025-11-10 02:02:29 -0800

    Remove reference to edit command from root help messages

--- GPTifier/src/main.cpp
+++ GPTifier/src/main.cpp
@@ -38,11 +38,9 @@ Commands:
   fine-tune      Manage fine tuning operations
   costs          Get OpenAI usage details
   chats          Manage chat completions uploaded to OpenAI [DEPRECATED]
-  edit           Edit one or more files according to a prompt [DEPRECATED]
   img            Generate an image from a prompt
 
 Try 'gpt <subcommand> [-h | --help]' for subcommand specific help.
-The 'edit' subcommand has been moved. See https://github.com/dsw7/FuncGraft for more information.
 )";
 
     fmt::print("-- GPTifier | v{}\n{}", PROJECT_VERSION, messages);
