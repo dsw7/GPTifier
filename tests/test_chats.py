62bb042f1d9474914d3744b1624de1a16a20e261
dsw7 <dds3443@hotmail.com>
2025-11-10 02:12:08 -0800

    Remove chats test case

--- tests/test_chats.py
+++
@@ -1,7 +0,0 @@
-from .extended_testcase import TestCaseExtended
-
-
-class TestChats(TestCaseExtended):
-    def test_throw_deprecation_exception(self) -> None:
-        proc = self.assertFailure("chats")
-        self.assertIn("This command has been deprecated.", proc.stderr)
