d6c411afef239d0652883fc6d46ab994b90ea360
dsw7 <dds3443@hotmail.com>
2025-12-11 04:33:46 -0800

    Return CurlResult from API call wrapper

--- GPTifier/src/networking/api_openai_user.hpp
+++ GPTifier/src/networking/api_openai_user.hpp
@@ -7,7 +7,7 @@
 namespace networking {
 CurlResult get_models();
 CurlResult delete_model(const std::string &model_id);
-std::string create_response(const std::string &post_fields);
+CurlResult create_response(const std::string &post_fields);
 CurlResult create_embedding(const std::string &post_fields);
 std::string upload_file(const std::string &filename, const std::string &purpose);
 std::string get_uploaded_files(bool sort_asc = true);
