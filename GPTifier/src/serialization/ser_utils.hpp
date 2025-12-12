7d9a7585268ebf48ab9a4a4a5cd2573199ee2609
dsw7 <dds3443@hotmail.com>
2025-12-12 01:34:16 -0800

    Remove unused code (linter suggestion)

--- GPTifier/src/serialization/ser_utils.hpp
+++ GPTifier/src/serialization/ser_utils.hpp
@@ -1,19 +1,9 @@
 #pragma once
 
 #include <json.hpp>
-
 #include <string>
 
 namespace serialization {
-
 nlohmann::json parse_json(const std::string &response);
 void throw_on_error_response(const std::string &response);
-
-struct Err {
-    long status_code = -1;
-    std::string errmsg;
-};
-
-Err unpack_error(const std::string &response, const long status_code);
-
 } // namespace serialization
