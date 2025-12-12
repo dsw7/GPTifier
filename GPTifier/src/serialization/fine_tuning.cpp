76ae827baad06a0aec1e9ed2e9e9976964e62d24
dsw7 <dds3443@hotmail.com>
2025-12-12 01:26:28 -0800

    Update error handling in serialization layer

--- GPTifier/src/serialization/fine_tuning.cpp
+++ GPTifier/src/serialization/fine_tuning.cpp
@@ -1,7 +1,7 @@
 #include "fine_tuning.hpp"
 
 #include "api_openai_user.hpp"
-#include "response_to_json.hpp"
+#include "ser_utils.hpp"
 #include "utils.hpp"
 
 #include <json.hpp>
@@ -11,41 +11,59 @@ namespace serialization {
 
 namespace {
 
-void unpack_fine_tuning_jobs(const nlohmann::json &json, FineTuningJobs &fine_tuning_jobs)
+FineTuningJobs unpack_fine_tuning_jobs(const std::string &response)
 {
-    for (const auto &entry: json["data"]) {
-        FineTuningJob job;
+    const nlohmann::json json = parse_json(response);
 
-        job.id = entry["id"];
-        job.created_at = entry["created_at"];
+    FineTuningJobs fine_tuning_jobs;
+    fine_tuning_jobs.raw_response = response;
 
-        if (not entry["finished_at"].is_null()) {
-            job.finished_at = utils::datetime_from_unix_timestamp(entry["finished_at"]);
+    try {
+        for (const auto &entry: json["data"]) {
+            FineTuningJob job;
+
+            job.id = entry["id"];
+            job.created_at = entry["created_at"];
+
+            if (not entry["finished_at"].is_null()) {
+                job.finished_at = utils::datetime_from_unix_timestamp(entry["finished_at"]);
+            }
+
+            if (not entry["estimated_finish"].is_null()) {
+                job.estimated_finish = utils::datetime_from_unix_timestamp(entry["estimated_finish"]);
+            }
+
+            fine_tuning_jobs.jobs.push_back(job);
         }
-
-        if (not entry["estimated_finish"].is_null()) {
-            job.estimated_finish = utils::datetime_from_unix_timestamp(entry["estimated_finish"]);
-        }
-
-        fine_tuning_jobs.jobs.push_back(job);
+    } catch (const nlohmann::json::exception &e) {
+        throw std::runtime_error(fmt::format("Failed to unpack response: {}", e.what()));
     }
+
+    return fine_tuning_jobs;
 }
 
 } // namespace
 
 FineTuningJobs get_fine_tuning_jobs(const int limit)
 {
-    const std::string response = networking::get_fine_tuning_jobs(limit);
-    FineTuningJobs fine_tuning_jobs = unpack_response<FineTuningJobs>(response, unpack_fine_tuning_jobs);
-    fine_tuning_jobs.raw_response = response;
-    return fine_tuning_jobs;
+    const auto result = networking::get_fine_tuning_jobs(limit);
+    if (not result) {
+        throw_on_error_response(result.error().response);
+    }
+
+    return unpack_fine_tuning_jobs(result->response);
 }
 
 std::string create_fine_tuning_job(const std::string &model, const std::string &training_file)
 {
     const nlohmann::json data = { { "model", model }, { "training_file", training_file } };
-    const std::string response = networking::create_fine_tuning_job(data.dump());
-    const nlohmann::json json = response_to_json(response);
+
+    const auto result = networking::create_fine_tuning_job(data.dump());
+    if (not result) {
+        throw_on_error_response(result.error().response);
+    }
+
+    const nlohmann::json json = parse_json(result->response);
 
     if (not json.contains("id")) {
         throw std::runtime_error("Malformed response. Missing 'id' key");
