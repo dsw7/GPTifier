f773d0d7df285e0758d468ccb263a32fb23d8171
dsw7 <dds3443@hotmail.com>
2026-01-07 01:32:37 -0800

    Migrate test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,28 +1,28 @@
-from .extended_testcase import TestCaseExtended
+import re
+import pytest
+import utils
+
+SEARCH_PATTERN = r"Creation time\s+Owner\s+Model ID"
 
 
-class TestModels(TestCaseExtended):
+@pytest.mark.parametrize("option", ["-h", "--help"])
+def test_help(option: str) -> None:
+    stdout = utils.assert_command_success("models", option)
+    assert "List available OpenAI or user models" in stdout
 
-    def test_help(self) -> None:
-        for option in ["-h", "--help"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess("models", option)
-                self.assertIn("List available OpenAI or user models", proc.stdout)
 
-    pattern = r"Creation time\s+Owner\s+Model ID"
+def test_models_openai() -> None:
+    stdout = utils.assert_command_success("models")
+    assert re.search(stdout, SEARCH_PATTERN) is not None
 
-    def test_models_openai(self) -> None:
-        proc = self.assertSuccess("models")
-        self.assertRegex(proc.stdout, self.pattern)
 
-    def test_models_user(self) -> None:
-        for option in ["-u", "--user"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess("models", option)
-                self.assertRegex(proc.stdout, self.pattern)
+@pytest.mark.parametrize("option", ["-u", "--user"])
+def test_models_user(option: str) -> None:
+    stdout = utils.assert_command_success("models", option)
+    assert re.search(stdout, SEARCH_PATTERN) is not None
 
-    def test_models_raw_json(self) -> None:
-        for option in ["-j", "--json"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess("models", option)
-                proc.load_stdout_to_json()
+
+@pytest.mark.parametrize("option", ["-j", "--json"])
+def test_models_raw_json(option: str) -> None:
+    stdout = utils.assert_command_success("models", option)
+    utils.load_stdout_to_json(stdout)
