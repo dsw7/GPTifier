fa2e7f682d23580a04b371c85f1325c407fac154
dsw7 <dds3443@hotmail.com>
2026-01-07 01:21:08 -0800

    Migrate test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,43 +1,40 @@
 from datetime import datetime
-from .extended_testcase import TestCaseExtended
+import pytest
+import utils
 
 
-class TestVersion(TestCaseExtended):
+def test_correct_version() -> None:
+    # I.e. ensure CMakeLists.txt project VERSION is updated
+    stdout = utils.assert_command_success("-v")
+    json = utils.load_stdout_to_json(stdout)
+    assert "version" in json
+    assert json["version"] == "1.7.0"
 
-    def test_correct_version(self) -> None:
-        # I.e. ensure CMakeLists.txt project VERSION is updated
-        proc = self.assertSuccess("-v")
-        stdout = proc.load_stdout_to_json()
-        self.assertIn("version", stdout)
-        self.assertEqual(stdout["version"], "1.7.0")
 
-    def test_version_version(self) -> None:
-        for option in ["-v", "--version"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess(option)
-                stdout = proc.load_stdout_to_json()
-                self.assertIn("version", stdout)
+@pytest.mark.parametrize("option", ["-v", "--version"])
+def test_version_version(option: str) -> None:
+    stdout = utils.assert_command_success(option)
+    json = utils.load_stdout_to_json(stdout)
+    assert "version" in json
 
-    def test_version_build_type(self) -> None:
-        for option in ["-v", "--version"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess(option)
-                stdout = proc.load_stdout_to_json()
-                self.assertIn("build_type", stdout)
-                self.assertEqual(stdout["build_type"], "Testing")
 
-    def test_version_build_date(self) -> None:
-        for option in ["-v", "--version"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess(option)
-                stdout = proc.load_stdout_to_json()
-                self.assertIn("build_date", stdout)
+@pytest.mark.parametrize("option", ["-v", "--version"])
+def test_version_build_type(option: str) -> None:
+    stdout = utils.assert_command_success(option)
+    json = utils.load_stdout_to_json(stdout)
+    assert "build_type" in json
+    assert json["build_type"] == "Testing"
 
-                build_date = datetime.strptime(
-                    stdout["build_date"], "%Y-%m-%dT%H:%M:%S"
-                )
-                now = datetime.now()
 
-                self.assertEqual(build_date.day, now.day)
-                self.assertEqual(build_date.month, now.month)
-                self.assertEqual(build_date.year, now.year)
+@pytest.mark.parametrize("option", ["-v", "--version"])
+def test_version_build_date(option: str) -> None:
+    stdout = utils.assert_command_success(option)
+    json = utils.load_stdout_to_json(stdout)
+    assert "build_date" in json
+
+    build_date = datetime.strptime(json["build_date"], "%Y-%m-%dT%H:%M:%S")
+    now = datetime.now()
+
+    assert build_date.day == now.day
+    assert build_date.month == now.month
+    assert build_date.year == now.year
