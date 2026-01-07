bdaaf9fe3edd9d33dff22d1a0e2662068b6d54b4
dsw7 <dds3443@hotmail.com>
2026-01-07 02:47:48 -0800

    Migrate test_embed.py

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -2,26 +2,89 @@ from dataclasses import dataclass
 from json import loads
 from pathlib import Path
 from tempfile import gettempdir
-from .extended_testcase import TestCaseExtended
+from typing import Generator
+import pytest
+import utils
+
+
+@pytest.mark.parametrize("option", ["-h", "--help"])
+def test_help(option: str) -> None:
+    stdout = utils.assert_command_success("embed", option)
+    assert "Get embedding representing a block of text" in stdout
+
+
+def test_missing_input_file() -> None:
+    stderr = utils.assert_command_failure("embed", "--read-from-file=/tmp/yU8nnkRs.txt")
+    assert "Unable to open '/tmp/yU8nnkRs.txt'" in stderr
+
+
+def test_empty_input() -> None:
+    stderr = utils.assert_command_failure("embed", "--input=")
+    assert "No input text provided anywhere" in stderr
+
+
+def test_empty_model() -> None:
+    stderr = utils.assert_command_failure("embed", "--input=foobar", "--model=")
+    assert "Model is empty" in stderr
+
+
+def test_empty_output_file() -> None:
+    stderr = utils.assert_command_failure("embed", "--input=foobar", "--output-file=")
+    assert "Could not write to file. Filename is empty" in stderr
+
+
+def test_empty_input_file() -> None:
+    stderr = utils.assert_command_failure(
+        "embed", "--read-from-file=", "--output-file=/tmp/results.txt"
+    )
+    assert "Could not read from file. Filename is empty" in stderr
+
+
+def test_non_existent_model() -> None:
+    stderr = utils.assert_command_failure("embed", "-i'What is 3 + 5?'", "-mfoobar")
+    assert (
+        "The model `foobar` does not exist or you do not have access to it." in stderr
+    )
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "dall-e-3",
+        "gpt-3.5-turbo",
+        "gpt-4",
+        "gpt-4.1",
+        "gpt-4o",
+        "o1",
+        "o3",
+        "tts-1",
+        "whisper-1",
+    ],
+)
+def test_wrong_models(model: str) -> None:
+    stderr = utils.assert_command_failure(
+        "embed", "--input='A foo that bars'", f"-m{model}"
+    )
+    assert "You are not allowed to generate embeddings from this model" in stderr
 
 
 @dataclass
-class Embedding:
+class _Embedding:
     embedding: list[float]
     model: str
     text: str
 
 
-def load_embedding(results_file: Path) -> Embedding:
+def _load_embedding(results_file: Path) -> _Embedding:
     with results_file.open() as f:
         data = loads(f.read())
 
-    return Embedding(
+    return _Embedding(
         model=data["model"], text=data["input"], embedding=data["embedding"]
     )
 
 
-def get_cosine_similarity(left: Embedding, right: Embedding) -> float:
+def _get_cosine_similarity(left: _Embedding, right: _Embedding) -> float:
     dot_p: float = 0
 
     for l, r in zip(left.embedding, right.embedding):
@@ -33,119 +96,64 @@ def get_cosine_similarity(left: Embedding, right: Embedding) -> float:
     return dot_p / (mag_l * mag_r)
 
 
-class TestEmbed(TestCaseExtended):
+@pytest.fixture
+def files_parallel() -> Generator[tuple[Path, Path], None, None]:
+    input_file = Path(__file__).resolve().parent / "test_embed" / "lorem.txt"
+    output_file = Path(gettempdir()) / "result.gpt"
+    yield input_file, output_file
 
-    def test_help(self) -> None:
-        for option in ["-h", "--help"]:
-            with self.subTest(option=option):
-                proc = self.assertSuccess("embed", option)
-                self.assertIn("Get embedding representing a block of text", proc.stdout)
-
-    def test_missing_input_file(self) -> None:
-        proc = self.assertFailure("embed", "--read-from-file=/tmp/yU8nnkRs.txt")
-        self.assertIn("Unable to open '/tmp/yU8nnkRs.txt'", proc.stderr)
-
-    def test_empty_input(self) -> None:
-        proc = self.assertFailure("embed", "--input=")
-        self.assertIn("No input text provided anywhere", proc.stderr)
-
-    def test_empty_model(self) -> None:
-        proc = self.assertFailure("embed", "--input=foobar", "--model=")
-        self.assertIn("Model is empty", proc.stderr)
-
-    def test_empty_output_file(self) -> None:
-        proc = self.assertFailure("embed", "--input=foobar", "--output-file=")
-        self.assertIn("Could not write to file. Filename is empty", proc.stderr)
-
-    def test_empty_input_file(self) -> None:
-        proc = self.assertFailure(
-            "embed", "--read-from-file=", "--output-file=/tmp/results.txt"
-        )
-        self.assertIn("Could not read from file. Filename is empty", proc.stderr)
+    if output_file.exists():
+        output_file.unlink()
 
 
-class TestEmbedIncompatibleModels(TestCaseExtended):
+@pytest.fixture
+def files_orthogonal() -> Generator[tuple[Path, Path], None, None]:
+    filename_1 = Path(gettempdir()) / "result_1.gpt"
+    filename_2 = Path(gettempdir()) / "result_2.gpt"
 
-    def test_non_existent_model(self) -> None:
-        proc = self.assertFailure("embed", "-i'What is 3 + 5?'", "-mfoobar")
-        self.assertIn(
-            "The model `foobar` does not exist or you do not have access to it.",
-            proc.stderr,
-        )
+    yield filename_1, filename_2
 
-    def test_wrong_models(self) -> None:
-        for model in [
-            "dall-e-3",
-            "gpt-3.5-turbo",
-            "gpt-4",
-            "gpt-4.1",
-            "gpt-4o",
-            "o1",
-            "o3",
-            "tts-1",
-            "whisper-1",
-        ]:
-            with self.subTest(model=model):
-                proc = self.assertFailure(
-                    "embed", "--input='A foo that bars'", f"-m{model}"
-                )
-                self.assertIn(
-                    "You are not allowed to generate embeddings from this model",
-                    proc.stderr,
-                )
+    if filename_1.exists():
+        filename_1.unlink()
+
+    if filename_2.exists():
+        filename_2.unlink()
 
 
-class TestEmbedCosineSimilarityIdentical(TestCaseExtended):
+def test_compute_cosine_similarities_parallel(
+    files_parallel: tuple[Path, Path],
+) -> None:
+    input_file, output_file = files_parallel
+    model = "text-embedding-3-small"
+    utils.assert_command_success(
+        "embed", f"-r{input_file}", f"-m{model}", f"-o{output_file}"
+    )
 
-    def setUp(self) -> None:
-        self.input_file = Path(__file__).resolve().parent / "test_embed" / "lorem.txt"
-        self.output_file = Path(gettempdir()) / "result.gpt"
+    embedding = _load_embedding(output_file)
 
-    def tearDown(self) -> None:
-        if self.output_file.exists():
-            self.output_file.unlink()
+    assert model == embedding.model
+    assert embedding.text in input_file.read_text()
 
-    def test_compute_cosine_similarities(self) -> None:
-        model = "text-embedding-3-small"
-        self.assertSuccess(
-            "embed", f"-r{self.input_file}", f"-m{model}", f"-o{self.output_file}"
-        )
-
-        embedding = load_embedding(self.output_file)
-
-        self.assertEqual(model, embedding.model)
-        self.assertEqual(embedding.text, self.input_file.read_text())
-
-        self.assertEqual(
-            len(embedding.embedding), 1536
-        )  # text-embedding-ada-002 dimension
-        self.assertAlmostEqual(
-            get_cosine_similarity(embedding, embedding), 1.00, places=2
-        )
+    assert len(embedding.embedding) == 1536  # text-embedding-ada-002 dimension
+    pytest.approx(_get_cosine_similarity(embedding, embedding), 1.00)
 
 
-class TestEmbedCosineSimilarityOrthogonal(TestCaseExtended):
+def test_compute_cosine_similarities_orthogonal(
+    files_orthogonal: tuple[Path, Path],
+) -> None:
+    filename_1, filename_2 = files_orthogonal
+    model = "text-embedding-ada-002"
 
-    def setUp(self) -> None:
-        self.filename_1 = Path(gettempdir()) / "result_1.gpt"
-        self.filename_2 = Path(gettempdir()) / "result_2.gpt"
+    text_1 = "The cat meowed softly."
+    utils.assert_command_success(
+        "embed", f"-i{text_1}", f"-m{model}", f"-o{filename_1}"
+    )
+    embedding_1 = _load_embedding(filename_1)
 
-    def tearDown(self) -> None:
-        if self.filename_1.exists():
-            self.filename_1.unlink()
+    text_2 = "Quantum physics is fascinating."
+    utils.assert_command_success(
+        "embed", f"-i{text_2}", f"-m{model}", f"-o{filename_2}"
+    )
+    embedding_2 = _load_embedding(filename_2)
 
-        if self.filename_2.exists():
-            self.filename_2.unlink()
-
-    def test_compute_cosine_similarities(self) -> None:
-        model = "text-embedding-ada-002"
-
-        text_1 = "The cat meowed softly."
-        self.assertSuccess("embed", f"-i{text_1}", f"-m{model}", f"-o{self.filename_1}")
-        embedding_1 = load_embedding(self.filename_1)
-
-        text_2 = "Quantum physics is fascinating."
-        self.assertSuccess("embed", f"-i{text_2}", f"-m{model}", f"-o{self.filename_2}")
-        embedding_2 = load_embedding(self.filename_2)
-
-        self.assertTrue(0.6 <= get_cosine_similarity(embedding_1, embedding_2) <= 0.8)
+    assert 0.6 <= _get_cosine_similarity(embedding_1, embedding_2) <= 0.8
