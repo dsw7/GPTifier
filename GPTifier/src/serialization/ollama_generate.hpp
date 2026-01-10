0745a49c0f613ad13f83c8e0b7c04282a8e115cb
dsw7 <dds3443@hotmail.com>
2026-01-10 00:36:01 -0800

    Fix type

--- GPTifier/src/serialization/ollama_generate.hpp
+++ GPTifier/src/serialization/ollama_generate.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <chrono>
-#include <ctime>
 #include <json.hpp>
 #include <string>
 
@@ -11,11 +10,11 @@ struct OllamaResponse {
     int input_tokens = 0;
     int output_tokens = 0;
     std::chrono::duration<float> rtt;
+    std::string created;
     std::string input;
     std::string model;
     std::string output;
     std::string raw_response;
-    std::time_t created = 0;
 };
 
 OllamaResponse create_ollama_response(const std::string &prompt, const std::string &model);
