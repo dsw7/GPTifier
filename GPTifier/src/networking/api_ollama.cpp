7728a1a61b6e0c42730a3b095d30ec8d6c25d599
dsw7 <dds3443@hotmail.com>
2026-01-17 04:40:47 -0800

    WIP ollama networking

--- GPTifier/src/networking/api_ollama.cpp
+++ GPTifier/src/networking/api_ollama.cpp
@@ -6,7 +6,7 @@
 
 namespace {
 
-std::string get_ollama_base_url()
+std::string get_ollama_base_url_()
 {
     static const std::string host = configs.host_ollama.value();
     static const int port = configs.port_ollama.value();
@@ -27,7 +27,7 @@ CurlResult generate_ollama_response(const std::string &post_fields)
     curl.append_header("Content-Type: application/json");
     curl_easy_setopt(handle, CURLOPT_HTTPHEADER, curl.get_headers());
 
-    const std::string url_generate = fmt::format("{}/generate", get_ollama_base_url());
+    const std::string url_generate = fmt::format("{}/generate", get_ollama_base_url_());
     curl_easy_setopt(handle, CURLOPT_URL, url_generate.c_str());
     curl_easy_setopt(handle, CURLOPT_POST, 1L);
     curl_easy_setopt(handle, CURLOPT_POSTFIELDS, post_fields.c_str());
@@ -47,7 +47,7 @@ CurlResult create_ollama_embedding(const std::string &post_fields)
     curl.append_header("Content-Type: application/json");
     curl_easy_setopt(handle, CURLOPT_HTTPHEADER, curl.get_headers());
 
-    const std::string url_embed = fmt::format("{}/embed", get_ollama_base_url());
+    const std::string url_embed = fmt::format("{}/embed", get_ollama_base_url_());
     curl_easy_setopt(handle, CURLOPT_URL, url_embed.c_str());
     curl_easy_setopt(handle, CURLOPT_POST, 1L);
     curl_easy_setopt(handle, CURLOPT_POSTFIELDS, post_fields.c_str());
